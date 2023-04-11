733c5427286ecbcddde6391728c65f5b935ec58d
Karol Bura <karol.bura544@gmail.com>
2023-04-11 05:16:58 +0100

    added login GET handler

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,36 @@
 const express = require('express')
+//file reader
+const fs = require('fs')
+//sorting out file paths
+const path = require('path')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("lsjdg")
+router.get('/:user-:password', (req, res) => {
+    fs.readFile('./data/users/users.txt', 'utf8', (err, data) => {
+        let rows = data.split(';')
+        let rows2 = [[]]
+        rows.forEach(row => {
+            for (let x = 0; x < rows.length; x++) {
+                rows2[x] = rows[x].split(',')
+            }
+        });
+
+        rows2.forEach(row => {
+            if (row[0] == req.params.user && row[2] == req.params.password) {
+                //login successful
+                correctRow = [...row]
+                res.send(correctRow)
+                // res.send(row[0] + ',' + row[1] + ',' + row[2] + ',' + row[3])
+            }
+        });
+        //login unsuccessful
+        res.send('Incorrect login.')
+      });
 })
+
+// router.param('password', (req, res, next, password) => {
+
+//     next()
+// })
 
 module.exports = router
