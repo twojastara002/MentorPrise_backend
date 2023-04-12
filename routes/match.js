8d404f438a172a53b61990b625b919cdc4ea596a
Karol Bura <karol.bura544@gmail.com>
2023-04-13 00:10:28 +0100

    remove match specyfing only one of the users

--- routes/match.js
+++ routes/match.js
@@ -58,6 +58,48 @@ router.get('/create/:u1-:u2', (req, res) => {
     addMatch(req, res, req.params.u1, req.params.u2)
 })
 
+//delete match with only one specified
+router.get('/remove/:u1', (req, res) => {
+    let u1 = req.params.u1
+
+    //if file is empty or doesn't exist then don't do anything
+    if (!fs.existsSync(filePath)) {
+        res.send('no matches exist')
+    } else {
+        //read in the whole file
+        fs.readFile('./data/matches/matches.txt', 'utf8', (err, data) => {
+            let x = -1
+            let matches = data.split(';')
+            //create a string and copy over all matches up to the one with the marked index
+            let matchesTMP = ''
+            matches.forEach(match => { //iterate all matches
+                matchArray = match.split(',')
+                //find which one to remove
+                if (matchArray[1] == u1  || matchArray[2] == u1) {
+                    x = matchArray[0]
+                } else { //copy over all others
+                    matchesTMP = matchesTMP + match + ';'
+                }
+            });
+            if (x == -1) {
+                res.send('not found')
+            } else {
+                //override the old file with the new one
+                //remove last semicolon
+                matchesTMP = matchesTMP.substring(0,matchesTMP.length-1)
+                fs.writeFile(filePath, matchesTMP, 'utf8', function(err) {
+                    if (err) { //something went wrong
+                        res.send('Match failed to remove.')
+                    } else { // no error
+                        res.send('Match removed successfully.')
+                    }
+                })
+            }
+        })   
+    }
+})
+
+//delete match with two users specified
 router.get('/remove/:u1-:u2', (req, res) => {
     let u1 = req.params.u1
     let u2 = req.params.u2
